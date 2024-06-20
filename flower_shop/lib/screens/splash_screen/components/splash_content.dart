92558f74163138d04f737229a68017bbb4f3a632
Trần Bảo Hân <tranbaohan250903@gmail.com>
2024-06-20 13:32:56 +0700

    sửa giao diện

--- flower_shop/lib/screens/splash_screen/components/splash_content.dart
+++ flower_shop/lib/screens/splash_screen/components/splash_content.dart
@@ -5,10 +5,9 @@ import '../../../constants.dart';
 class SplashContent extends StatefulWidget {
   const SplashContent({
     Key? key,
-    this.text,
     this.image,
   }) : super(key: key);
-  final String? text, image;
+  final String? image;
 
   @override
   State<SplashContent> createState() => _SplashContentState();
@@ -20,18 +19,14 @@ class _SplashContentState extends State<SplashContent> {
     return Column(
       children: <Widget>[
         const Spacer(),
-        const Text(
-          "HOA TƯƠI",
-          style: TextStyle(
-            fontSize: 32,
-            color: kPrimaryColor,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-        Text(
-          widget.text!,
-          textAlign: TextAlign.center,
-        ),
+        // const Text(
+        //   "HOA TƯƠI",
+        //   style: TextStyle(
+        //     fontSize: 32,
+        //     color: kPrimaryColor,
+        //     fontWeight: FontWeight.bold,
+        //   ),
+        // ),
         // const Spacer(flex: 1),
         Image.asset(
           widget.image!,
