eb29d78db9a4950b8a94a93bbb2f77f275179de4
Trọng Trần <tranhoangtrong2003@gmail.com>
2024-06-22 22:54:06 +0700

    bo góc hình ở phía bên trái

--- flower_shop/lib/screens/cart_screen/cart_item.dart
+++ flower_shop/lib/screens/cart_screen/cart_item.dart
@@ -55,78 +55,106 @@ class _CartItemWidgetState extends State<CartItemWidget> {
               // Hình ảnh sản phẩm
               Container(
                 decoration: BoxDecoration(
-                  color: kcolorminor.withOpacity(0.1), // Màu nền tím, có thể điều chỉnh theo ý của bạn
-                  // Để bo tròn viền
+                  color: kcolorminor.withOpacity(0.1), // Màu nền, có thể điều chỉnh theo ý của bạn
+                  borderRadius: const BorderRadius.only(
+                    topLeft: Radius.circular(10), // Bo tròn góc bên trái trên
+                    bottomLeft: Radius.circular(10), // Bo tròn góc bên trái dưới
+                  ),
                 ),
                 width: 140,
                 height: 140,
-                child: Image.asset(
-                  widget.cartItem.img,
-                  fit: BoxFit.cover,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(10), // Để bo tròn góc bên trái trên
+                    bottomLeft: Radius.circular(10), // Để bo tròn góc bên trái dưới
+                  ),
+                  child: Image.asset(
+                    widget.cartItem.img,
+                    fit: BoxFit.cover,
+                  ),
                 ),
               ),
+
               const SizedBox(width: 16),
               Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(widget.cartItem.name,
-                        style: const TextStyle(fontSize: 18)),
-                    const SizedBox(height: 5),
-                    Text(
-                      "\$${(widget.cartItem.price * widget.cartItem.quantity).toStringAsFixed(2)}",
-                    ),
-                    const SizedBox(height: 5),
-                    Row(
-                      children: [
-                        Container(
-                          decoration: const BoxDecoration(
-                            color: Colors.white,
-                            shape: BoxShape.circle,
-                            boxShadow: [
-                              BoxShadow(
-                                color: Colors.black26,
-                                blurRadius: 1,
-                                offset: Offset(0, 2),
-                              ),
-                            ],
-                          ),
-                          child: SizedBox(
-                            width: 35,
-                            child: IconButton(
-                              iconSize: 20,
-                              icon: const Icon(Icons.remove_circle_outline),
-                              onPressed: _decreaseQuantity,
-                            ),
-                          ),
-                        ),
-                        const SizedBox(width: 10),
-                        Text(widget.cartItem.quantity.toString(), style: const TextStyle(fontSize: 18)),
-                        const SizedBox(width: 10),
-                        Container(
-                          decoration: const BoxDecoration(
-                            color: Colors.white,
-                            shape: BoxShape.circle,
-                            boxShadow: [
-                              BoxShadow(
-                                color: Colors.black26,
-                                blurRadius: 1,
-                                offset: Offset(0, 2),
-                              ),
-                            ],
-                          ),
-                          child: SizedBox(
-                            width: 35,
-                            child: IconButton(
-                              iconSize: 20,
-                              icon: const Icon(Icons.add_circle_outline),
-                              onPressed: _increaseQuantity,
-                            ),
-                          ),
+                child: ClipRRect(
+                  borderRadius: const BorderRadius.only(
+                    topRight: Radius.circular(20), // Bo tròn góc bên phải trên
+                    bottomRight: Radius.circular(20), // Bo tròn góc bên phải dưới
+                  ),
+                  child: Container(
+                    padding: EdgeInsets.all(10),
+                    decoration: const BoxDecoration(
+                      color: Colors.white,
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black26,
+                          blurRadius: 1,
+                          offset: Offset(0, 2),
                         ),
                       ],
                     ),
-                  ],
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(widget.cartItem.name, style: const TextStyle(fontSize: 18)),
+                        const SizedBox(height: 5),
+                        Text(
+                          "\$${(widget.cartItem.price * widget.cartItem.quantity).toStringAsFixed(2)}",
+                        ),
+                        const SizedBox(height: 5),
+                        Row(
+                          children: [
+                            Container(
+                              decoration: const BoxDecoration(
+                                color: Colors.white,
+                                shape: BoxShape.circle,
+                                boxShadow: [
+                                  BoxShadow(
+                                    color: Colors.black26,
+                                    blurRadius: 1,
+                                    offset: Offset(0, 2),
+                                  ),
+                                ],
+                              ),
+                              child: SizedBox(
+                                width: 35,
+                                child: IconButton(
+                                  iconSize: 20,
+                                  icon: const Icon(Icons.remove_circle_outline),
+                                  onPressed: _decreaseQuantity,
+                                ),
+                              ),
+                            ),
+                            const SizedBox(width: 10),
+                            Text(widget.cartItem.quantity.toString(), style: const TextStyle(fontSize: 18)),
+                            const SizedBox(width: 10),
+                            Container(
+                              decoration: const BoxDecoration(
+                                color: Colors.white,
+                                shape: BoxShape.circle,
+                                boxShadow: [
+                                  BoxShadow(
+                                    color: Colors.black26,
+                                    blurRadius: 1,
+                                    offset: Offset(0, 2),
+                                  ),
+                                ],
+                              ),
+                              child: SizedBox(
+                                width: 35,
+                                child: IconButton(
+                                  iconSize: 20,
+                                  icon: const Icon(Icons.add_circle_outline),
+                                  onPressed: _increaseQuantity,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
                 ),
               ),
             ],
