5ad2abe82c1eb515c8867a4f2d93745253c4581a
Trần Bảo Hân <tranbaohan250903@gmail.com>
2024-06-24 19:41:52 +0700

    splash (bo sung)

--- flower_shop/lib/screens/splash_screen/components/splash_content.dart
+++ flower_shop/lib/screens/splash_screen/components/splash_content.dart
@@ -1,39 +1,23 @@
 import 'package:flutter/material.dart';
 
-import '../../../constants.dart';
+class SplashContent extends StatelessWidget {
+  final String? image;
 
-class SplashContent extends StatefulWidget {
+  // ignore: use_super_parameters
   const SplashContent({
     Key? key,
     this.image,
   }) : super(key: key);
-  final String? image;
 
-  @override
-  State<SplashContent> createState() => _SplashContentState();
-}
-
-class _SplashContentState extends State<SplashContent> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        // const Spacer(),
-        //  const Text(
-        //    "",
-        //    style: TextStyle(
-        //      fontSize: 32,
-        //      color: kPrimaryColor,
-        //      fontWeight: FontWeight.bold,
-        //    ),
-        //  ),
-        const Spacer(flex: 1),
-        Image.asset(
-          widget.image!,
-          height: 400,
-          width: 400,
-        ),
-      ],
+    return FittedBox(
+      fit: BoxFit.cover,
+      child: Image.asset(
+        image!,
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+      ),
     );
   }
 }
